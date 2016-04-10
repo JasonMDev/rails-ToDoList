6aac442bdd9d62d16cf3b99c4bf681b50784d7c1
JasonMDev <jasonmansdev@gmail.com>
2016-04-11 00:06:52 +0100

    Added user scaffold, devise and cancan to get it working.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}, :controllers => { :registrations => "user_registrations"}
+  devise_for :users
+  
   root 'users#index'
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
